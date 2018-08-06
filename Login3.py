57f9b7ad8635a255591fd29d3a7201604abe2178
FelicxFoster <33318659+FelicxFoster@users.noreply.github.com>
2018-08-06 18:50:48 +0800

    Update Login3.py

--- Login3.py
+++ Login3.py
@@ -8,8 +8,8 @@ import os
 
 from Lib.facecpp import *
 from Lib.cv2fn import take_picture
-from SetAccount import existFacesetName
-from SetAccount import facedict
+#from SetAccount1 import existFacesetName
+from SetAccount1 import facedict
 
 def login():
 #    if not os.path.exists('tmp'): os.mkdir('tmp')
@@ -17,25 +17,32 @@ def login():
     capInput = cv2.VideoCapture(0)
     if not capInput.isOpened(): print('Capture failed because of camera')
     while 1:
-        print(existFacesetName)
-        existFaceName=[]
-        for x in existFacesetName:
-            existFaceName.append(get_face_list(x))
-        print (existFaceName)
+#        print(existFacesetName)
+#        existFaceName=[]
+#        for x in existFacesetName:
+#            existFaceName.append(get_face_list(x))
+#        print (existFaceName)
         ret, img = capInput.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray, 1.3, 5)
         cv2.imwrite("/home/lalala/face测试/wt.jpg",img,[int(cv2.IMWRITE_JPEG_QUALITY),80])
         pictureId = upload_img1("/home/lalala/face测试/wt.jpg")
         if len(pictureId) > 0:
-            for x in existFaceName :
-                for i in x:
+#            for x in existFaceName :
 #                    print (i)
-                    facetoken,confidence = search(pictureId,i)
-                    if confidence > 75:
-                        print(facetoken)
-                        if facetoken in facedict.keys():
-                            state=facedict[facetoken]
+				facetoken,confidence = search(pictureId,'gakki')
+				if confidence > 75:
+					print(facetoken)
+					if facetoken in facedict.keys():
+#						state=facedict.get[facetoken]
+						print('ok')
+						print(confidence)
+					else:
+						print('failed')
+						print(confidence)
+				else:
+					print('no')
+					print(confidence)
         break
     capInput.release()
 
@@ -44,4 +51,4 @@ def login():
 
 
 if __name__ == '__main__':
-    login()
+	login()
