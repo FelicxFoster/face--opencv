44a57c5dd9b319ba77645cecb4e7db891e56607a
FelicxFoster <33318659+FelicxFoster@users.noreply.github.com>
2018-08-07 09:28:54 +0800

    Update Login3.py

--- Login3.py
+++ Login3.py
@@ -7,48 +7,39 @@ import uniout
 import os
 
 from Lib.facecpp import *
-from Lib.cv2fn import take_picture
-#from SetAccount1 import existFacesetName
+#from Lib.cv2fn import take_picture
 from SetAccount1 import facedict
+from Lib.cv2fn import detect_face
 
 def login():
-#    if not os.path.exists('tmp'): os.mkdir('tmp')
-    faceCascade = cv2.CascadeClassifier("/home/lalala/opencv-3.2.0/data/haarcascades/haarcascade_frontalface_default.xml")
-    capInput = cv2.VideoCapture(0)
-    if not capInput.isOpened(): print('Capture failed because of camera')
-    while 1:
-#        print(existFacesetName)
-#        existFaceName=[]
-#        for x in existFacesetName:
-#            existFaceName.append(get_face_list(x))
-#        print (existFaceName)
-        ret, img = capInput.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-        cv2.imwrite("/home/lalala/face测试/wt.jpg",img,[int(cv2.IMWRITE_JPEG_QUALITY),80])
-        pictureId = upload_img1("/home/lalala/face测试/wt.jpg")
-        if len(pictureId) > 0:
-#            for x in existFaceName :
-#                    print (i)
-				facetoken,confidence = search(pictureId,'gakki')
-				if confidence > 75:
-					print(facetoken)
-					if facetoken in facedict.keys():
+#	faceCascade = cv2.CascadeClassifier("/home/lalala/opencv-3.2.0/data/haarcascades/haarcascade_frontalface_default.xml")
+	capInput = cv2.VideoCapture(0)
+	capInput.set(3,640) # set Width
+	capInput.set(4,480) # set Height
+	if not capInput.isOpened(): print('Capture failed because of camera')
+	while 1:
+		ret, img = capInput.read()
+		cv2.imshow('Image',img)
+		key = cv2.waitKey(1) & 0xFF
+		face = detect_face(img)
+		if not face:
+			print('No face found')
+		else:
+			cv2.imwrite("/home/lalala/face测试/wt.jpg",img,[int(cv2.IMWRITE_JPEG_QUALITY),80])
+		pictureId = upload_img1("/home/lalala/face测试/wt.jpg")
+		if len(pictureId) > 0:
+			facetoken,confidence = search(pictureId,'gakki')
+			if confidence > 75:
+				print(facetoken)
+				if facetoken in facedict.keys():
 #						state=facedict.get[facetoken]
-						print('ok')
-						print(confidence)
-					else:
-						print('failed')
-						print(confidence)
-				else:
-					print('no')
+					print('ok')
 					print(confidence)
-        break
-    capInput.release()
-
-
-
-
-
-if __name__ == '__main__':
-	login()
+				else:
+					print('failed')
+					print(confidence)
+			else:
+				print('no')
+				print(confidence)
+		break
+	capInput.release()
