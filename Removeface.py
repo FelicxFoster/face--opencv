4647bacc5ed9ed72fba858bd892ce52f5695b48f
FelicxFoster <33318659+FelicxFoster@users.noreply.github.com>
2018-08-07 16:52:11 +0800

    Update Removeface.py

--- Removeface.py
+++ Removeface.py
@@ -9,16 +9,17 @@ while 1:
 #        '\n'.join(['* ' + outer_id for outer_id in existFacesetName]) + '\nEnd of List')
 
 
-    existFacesetName = get_face_list('gakki')
-    print('Here is the list of facetoken recorded:\n' +
+	existFacesetName = get_face_list('myface')
+	print('Here is the list of facetoken recorded:\n' +
         '\n'.join(['* ' + face_tokens for face_tokens in existFacesetName]) + '\nEnd of List')
 
 
-    sure = input('Do you want to delete the face?(y or n ) ')
-    if sure == 'y':
-        name = input('What\'s the name of the face you want to delete?(q to exit) ')
-        removeface(name,'gakki')
-        print('you have deleted the [%s]'%name)
+	sure = raw_input('Do you want to delete the face?(y or n ) ')
+	if sure == 'y':
+		name = raw_input('What\'s the name of the face you want to delete?(q to exit) ')
+		removeface(name,'myface')
+#		print(removeface(name,'myface'))
+		print('you have deleted the [%s]'%name)
         break
-    if sure == 'n':
-        break
+	if sure == 'n':
+		break
