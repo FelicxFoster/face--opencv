e306855611d01fc67bf799e7223bf5dee7426cc5
FelicxFoster <33318659+FelicxFoster@users.noreply.github.com>
2018-08-08 11:31:52 +0800

    Update DeletAccoynt.py

--- DeletAccoynt.py
+++ DeletAccoynt.py
@@ -6,11 +6,11 @@ from Lib.cv2fn import take_picture
 while 1:
 	existFacesetName = [facesets['outer_id'] for facesets in get_faceset_list()]
 	print('Here is the list of faceset recorded:\n' +
-        '\n'.join(['* ' + outer_id for outer_id in existFacesetName]) + '\nEnd of List')
+        	'\n'.join(['* ' + outer_id for outer_id in existFacesetName]) + '\nEnd of List')
 
-	sure = raw_input('Do you want to delete the account?(y or n ) ')
+	sure = input('Do you want to delete the account?(y or n ) ')
 	if sure == 'y':
-		name = raw_input('What\'s the name of the account you want to delete?(q to exit) ')
+		name = input('What\'s the name of the account you want to delete?(q to exit) ')
 		delete_faceset(name)
 		print('you have deleted the [%s]'%name)
 		break
