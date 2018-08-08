2e1a6e9208f17b0395eb6f2e97e1556563b023fb
FelicxFoster <33318659+FelicxFoster@users.noreply.github.com>
2018-08-08 11:34:23 +0800

    Update Removeface.py

--- Removeface.py
+++ Removeface.py
@@ -4,9 +4,6 @@ from Lib.facecpp import *
 from Lib.cv2fn import take_picture
 
 while 1:
-#	existFacesetName = [facesets['outer_id'] for facesets in get_faceset_list()]
-#	print('Here is the list of faceset recorded:\n' +
-#        '\n'.join(['* ' + outer_id for outer_id in existFacesetName]) + '\nEnd of List')
 
 
 	existFacesetName = get_face_list('myface')
@@ -14,12 +11,15 @@ while 1:
         '\n'.join(['* ' + face_tokens for face_tokens in existFacesetName]) + '\nEnd of List')
 
 
-	sure = raw_input('Do you want to delete the face?(y or n ) ')
+	sure = input('Do you want to delete the face?(y or n ) ')
 	if sure == 'y':
-		name = raw_input('What\'s the name of the face you want to delete?(q to exit) ')
+		name = input('What\'s the name of the face you want to delete?(q to exit) ')
 		removeface(name,'myface')
 #		print(removeface(name,'myface'))
 		print('you have deleted the [%s]'%name)
         break
 	if sure == 'n':
 		break
+
+if __name__ == '__main__':
+	remove()
